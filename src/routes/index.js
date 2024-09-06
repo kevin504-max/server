bf39b74aa986e97057ea1714fff5819f471e12a9
kevin504-max <kevinbrito2012@gmail.com>
2024-09-05 21:09:45 -0300

    feat: make index dynamic router file to read all router files

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const router = require('express').Router();
+
+const routesDirectory = __dirname;
+const routeFiles = fs.readdirSync(routesDirectory);
+
+const camelToSnake = (str) => {
+    return str.replace(/[A-Z]/g, (letter, index) => {
+        return index === 0 ? letter.toLowerCase() : `_${letter.toLowerCase()}`;
+    });
+};
+
+routeFiles.forEach(file => {
+    if (file === 'index.js') return;
+    const route = require(path.join(routesDirectory, file));
+    let prefix = file.split('Routes.js')[0]
+    prefix = camelToSnake(prefix);
+    prefix.toLowerCase();
+
+    router.use(`/${prefix}`, route);
+});
+
+module.exports = router;
